e2b9041bf15d754bc6ae4e7938f9f4b144e6da99
Yaakov Weizman <yaakov.weizman@novideasoft.com>
2020-07-08 22:42:13 +0300

    Vercel

---
+++ debug.cmd
@@ -0,0 +1,9 @@
+::Run the-english-game on Windows with Command Prompt
+::SET DEBUG=the-english-game:* & npm run start
+SET DEBUG=the-english-game:* & npm run debug
+
+:: Run the-english-game on Windows with PowerShell
+::SET DEBUG=the-english-game:* | npm start
+
+:: # Run the-english-game on Linux/macOS
+::DEBUG=the-english-game:* npm start
